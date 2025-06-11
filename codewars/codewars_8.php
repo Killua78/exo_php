ad59716e094d2c1265695d97d69fc91df6608b93
Killua78 <nacimeboubekeur@gmail.com>
2025-06-11 20:39:15 +0200

    codewars 8 done

--- codewars/codewars_8.php
+++ codewars/codewars_8.php
@@ -1,8 +1,9 @@
 <?php 
 
+$a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+$a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+
 function comp($a1, $a2) {
-    $a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-    $a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
     if($a1 === NULL || $a2 === NULL){
         return false;
     }
@@ -13,11 +14,23 @@ function comp($a1, $a2) {
         return false;
     }
 
-    $square = array_map(function($value){
-        return pow($value, 2);
-    }, $a1);
-    print_r($square);
+    $squareRootB = array_map('sqrt', $a2);
+    // print_r($squareRootB);
+    $aSorted = $a1;
+    sort($aSorted);
+    // print_r($a1);
+    $bSorted = $squareRootB;
+    sort($bSorted);
+    // print_r($squareRootB);
+
+    if($aSorted == $bSorted){
+        echo "True";
+        return true;
+    }else{
+        echo "False";
+        return false;
+    }
 }
 
-echo comp($a1, $a2);
+comp($a1, $a2);
 ?>
